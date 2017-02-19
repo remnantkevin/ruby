fa89b9a58ac9afd4f7bf87e9b87d3fa599082959
kevin <skiimilk@gmail.com>
2017-02-19 04:00:04 +0200

    Date time for bleet

--- activerecord/bleeter.rb
+++ activerecord/bleeter.rb
@@ -5,17 +5,44 @@ ActiveRecord::Base.logger = Logger.new(File.open('bleeter.log', 'w'))
 
 ActiveRecord::Base.establish_connection(
   :adapter  => 'sqlite3',
-  :database => 'bleeter.db'
+  :database => 'test2.db'
 )
 
-class User <  ActiveRecord::Base
+class User < ActiveRecord::Base
   has_many :bleets
 end
 
 class Bleet < ActiveRecord::Base
+  #? where these two go?
   belongs_to :user
-
   validates :message, length: {maximum: 160}
+
+  # where go in relation to validates?
+  # use blocks instead?
+  # after_initialize :after_initialize
+  before_save :before_save
+
+  # The after_initialize callback is triggered every time a new object of the class is initialized.
+  # if you wanna mess around with a bleet before saving it then this wouldn't work; but it also wouln't work whenever you read and store from the db cuz .new will be called
+  # private
+  #   def after_initialize
+  #     p 'Hello there'
+  #   end
+
+  private
+    def before_save
+      p 'hello'
+      # how do this from within model as opposed to in running part of file?
+      @bleeted_at = "hi"
+    end
+
+
+  # def initialize(user, message)
+  #   @user = user
+  #   @message = message
+  #   @bleeted_at = DateTime.now
+  # end
+
 end
 
 def user
@@ -34,7 +61,6 @@ end
 def bleet
   @bleet ||= Bleet.new(
     user: user,
-    message: 'this is my first bleet',
-    bleeted_at: DateTime.now
+    message: 'this is my first bleet'
   )
 end
--- activerecord/bleeter.sql
+++ activerecord/bleeter.sql
@@ -15,3 +15,15 @@ CREATE TABLE IF NOT EXISTS bleets (
  bleeted_at text NOT NULL,
  FOREIGN KEY (user_id) REFERENCES users (user_id)
 );
+
+-- CREATE TABLE IF NOT EXISTS bleets (
+--  bleet_id integer PRIMARY KEY,
+--  user_id integer NOT NULL,
+--  message text NOT NULL,
+--  bleeted_at text DEFAULT (DATETIME('now')),
+--  FOREIGN KEY (user_id) REFERENCES users (user_id)
+-- );
+
+-- CREATE TABLE IF NOT EXISTS tbl (d1 text, d2 text DEFAULT (DATETIME('now')));
+-- insert into tbl (d1) values ("hello");
+-- select * from tbl;
