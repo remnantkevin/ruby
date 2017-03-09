802b645e5181cb073690321d88308e4a9efc5f55
Kevin Elliott <skiimilk@gmail.com>
2017-03-09 02:09:05 +0200

    Add search for vleats by specific user

--- bleater/bleater/bleater.rb
+++ bleater/bleater/bleater.rb
@@ -14,6 +14,10 @@ module Bleater
     # why is this run from where init.rb is?
     def initialize
       #p Dir["*.rb"]
+
+      #? what inital set to?
+      @current_user = nil
+
       # do these exist beyond the scope of this init function?
       ActiveRecord::Base.logger = Logger.new(File.open('/home/kevin/webdev/ruby/bleater/database/bleater.log', 'w'))
 
@@ -26,6 +30,9 @@ module Bleater
 
     def launch
       #p Dir["*.rb"]
+
+      # Bleat.create(user_id: 1, message: "hbhbhbhbhb #happybirthday #kevin")
+
       introduction
       # action loop
       input = nil
@@ -34,6 +41,7 @@ module Bleater
         puts '1. Login'
         puts '2. Sign up'
         puts '3. View all Bleater users'
+        puts '4. View bleats by user'
         puts 'Q. Quit'
         print '> '
         input = gets.chomp
@@ -63,7 +71,10 @@ module Bleater
         signup_form
       when '3'
         puts "\n==== Users ===="
-        view_users
+        view_all_users
+      when '4'
+        puts "\n==== Find Bleats ===="
+        find_bleats_form
       end
     end
 
@@ -126,7 +137,7 @@ module Bleater
       print "\n4) Username: "
       username = gets.chomp.strip
       while valid_signup_form_input?(username, :u)
-        puts "\nThe username you entered is already in use or not valid. Usernames must be netween 5 and 15 characters long, and can contain only alphabetic characters and underscores. For example, rem_nant. PLease try again."
+        puts "\nThe username you entered is already in use or not valid. Usernames must be netween 5 and 15 characters long, and can contain only alphabetic characters and underscores. For example, rem_nant. Please try again."
         print 'Username: '
         username = gets.chomp.strip
       end
@@ -137,10 +148,11 @@ module Bleater
         print 'Password: '
         password = gets.chomp
       end
-      signup(name, surname, email, username, password)
+      signup_new_user(name, surname, email, username, password)
     end
 
     # would there be a better way of doing this function where you could go input.valid.....?
+    # would this be a validate method?
     def valid_signup_form_input?(input, type)
       # reason to do this with not symbols or vv?
       # better to use =~ somehow?
@@ -159,7 +171,7 @@ module Bleater
       end
     end
 
-    def signup(name, surname, email, username, password)
+    def signup_new_user(name, surname, email, username, password)
       # have to be in column order?
       # !! what if fail?
       User.create(first_name: name, last_name: surname, email: email, username: username, password: password)
@@ -168,9 +180,50 @@ module Bleater
 
     # !! use same one with optional input for viewing one or many
     # always select in same order twice if no change to db?
-    def view_users
-      # get all user so can interact after viewing them? all_users
-      User.select(:username).each_with_index {|user, index| puts "#{index+1}. #{user.username}"}
+    def view_all_users
+      # !! get all user so can interact after viewing them? all_users
+      # User.select(:username).each_with_index {|user, index| puts "#{index+1}. #{user.username}"}
+      #??????
+      User.select(:username).each {|user| puts user.username}
+    end
+
+    # should these say display .... for more description?
+    def find_bleats_form
+      puts 'Search for bleats made by someone'
+      puts '----'
+      print 'Username: '
+      username = gets.chomp.strip
+      # couldnt reuse valid method -- better way? && error message not reused..
+      #? the regex is quicker than searching if exists?
+      while !username.match(/^\w{5,15}$/) && !User.exists?(username: username)
+        puts "\nThat username does not exist. Usernames must be netween 5 and 15 characters long, and can contain only alphabetic characters and underscores. For example, rem_nant. Please try again."
+        print 'Username: '
+        username = gets.chomp.strip
+      end
+
+      #! not just form now -- handled by method that called form method? should the form method return the answer/input
+      #? use ifs?
+      display_bleats_by_user(username)
+    end
+
+    #! expand to find similarly names users?
+    # def search_for_user(username)
+    #   User.find_by(username: username)
+    # end
+
+    #? do you still check for nil if somewhere else you've made it not possible to give nil?
+    def display_bleats_by_user(username)
+        #? is it woth it to select :message and then use where ... instead of fetching all attributes and storing them?
+        #? faster/better way of doing the following?
+        bleats = Bleat.where(user_id: User.find_by(username: username).user_id) # can't use find_by as that returns the first one
+        # p bleats
+
+        #?! am i able to iterate over activerecordrelation becuase its enuerable? look it up. is it somethign that contains an array or is it an array?
+        #? is there a way of returning just an array of objects?
+        bleats.each do |bleat|
+          puts bleat.message
+        end
+
     end
 
   end
